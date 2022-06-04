7c8185dd60bd640bb670acd38f45f7d59aaca6d7
slicknickicarus <nickbpankuch@gmail.com>
2022-06-04 14:44:15 -0400

    Initial version defined.

---
+++ EventManager/Conditions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Conditions
+{
+    public static class Conditional
+    {
+        private static bool example1 = true;
+        private static bool example2 = true;
+        public static Func<bool>[] ArrayAll = 
+        {
+            () => example1 == example2,
+            () => example2 == example1
+        };
+    }
+    
+    // Conditions can be added in the array above if they can be defined statically. A function is provided to add them dynamically.
+}
---
+++ EventManager/EventTypes.cs
@@ -0,0 +1,14 @@
+﻿namespace EventTypes
+{
+    public enum EventType1
+    {
+        
+    }
+
+    public enum EventType2
+    {
+        
+    }
+    
+    // Add ad nauseam. As long as the Enums are defined within the EventTypes namespace, they will be included in the dictionary initialization. 
+}
